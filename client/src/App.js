1c225150e543a0696894c7ec88ac21c1797812d5
alomatee@yahoo.com <alomatee@yahoo.com>
2023-10-31 19:12:45 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Real Estate</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
